29fe4c2b32f6a72a12c6063db590c61958fa5d8e
DmitriiSokolov <Tazeyl@yandex.ru>
2026-01-12 13:49:59 +0300

    init commit

---
+++ src/test/java/sokolov/spring/finalassignment/FinalassignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package sokolov.spring.finalassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalassignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
