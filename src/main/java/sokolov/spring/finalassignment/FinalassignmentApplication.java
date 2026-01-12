815fad2d1ad37ad5a22478138d2b5adc2d97cac0
DmitriiSokolov <Tazeyl@yandex.ru>
2026-01-12 14:18:50 +0300

    event-manager-openapi

--- src/main/java/sokolov/spring/finalassignment/FinalassignmentApplication.java
+++ src/main/java/sokolov/spring/finalassignment/FinalassignmentApplication.java
@@ -1,13 +1,11 @@
-package sokolov.spring.finalassignment;
+package sokolov.spring.finalassignment;  // ← именно этот package
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FinalassignmentApplication {
-
+public class FinalassignmentApplication {  // ← именно такое имя класса
     public static void main(String[] args) {
         SpringApplication.run(FinalassignmentApplication.class, args);
     }
-
 }
